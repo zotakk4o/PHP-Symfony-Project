81b672f1ee2a845b392ecb53da768d0dfbe8f6be
undefined <zotak3105@gmail.com>
2017-04-02 12:11:31 +0300

    Revert "Flash Massages System Added"
    This reverts commit d5cc635d028ef608a339763344eda49f347172ed.

--- Vehturiinik/src/VehturiinikShopBundle/Controller/ShoppingCartController.php
+++ Vehturiinik/src/VehturiinikShopBundle/Controller/ShoppingCartController.php
@@ -126,8 +126,6 @@ class ShoppingCartController extends Controller
         }
         $session->set('products', $products);
 
-        $this->addFlash('notice', "$productName successfully added to your cart!");
-
         return $this->redirectToRoute('view_products_in_category',['id' => $product->getCategory()->getId()]);
 
 
